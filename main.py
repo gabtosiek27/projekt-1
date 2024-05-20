01189bd148180d01d4bd4c9aa4807745262789b5
Gabtosiek27 <gabtosiek27@users.noreply.github.com>
2024-05-20 21:45:41 +0200

    Update main.py with save to xml

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import json
+import yaml
+import xmltodict
 
 source_file_name = sys.argv[1]
 dest_file_name = sys.argv[2]
@@ -23,3 +25,10 @@ if dest_file_name.endswith('.yaml'):
 if source_file_name.endswith('.xml'):
   xml_string=source_file.read()
   python_dict=xmltodict.parse(xml_string)
+
+if dest_file_name.endswith('.xml'):
+  with open(dest_file_name, 'w') as dest_file:
+    xml_string=xmltodict.unparse(python_dict,pretty=True)
+    dest_file.write(xml_string)
+
+print(f"Copying file {source_file_name} to {dest_file_name}. Done.")
