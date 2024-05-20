2dc6c11febe76acc245dcdbcb82b24676fac90cc
Gabtosiek27 <gabtosiek27@users.noreply.github.com>
2024-05-20 21:37:42 +0200

    Update main.py with store json

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ source_file=open(source_file_name,"r")
 
 if source_file_name.endswith('.json'):
   python_dict=json.load(source_file)
+
+if dest_file_name.endswith('.json'):
+  with open(dest_file_name, 'w') as dest_file:
+    json.dump(python_dict, dest_file, indent=4)
